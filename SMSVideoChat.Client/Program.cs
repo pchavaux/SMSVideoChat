ae5a4ef6a40f5ca3973fc13eca8d8e6dd8e9c370
Paul Chavaux <paul.chavaux@outlook.com>
2025-01-15 13:48:26 -0600

    Add HubConnectionBuilder

--- SMSVideoChat.Client/Program.cs
+++ SMSVideoChat.Client/Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
 using SMSVideoChat.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
+builder.Services.AddSingleton(sp =>
+{
+    return new HubConnectionBuilder()
+        .WithUrl("https://smsvideochat.azurewebsites.net/chatHub") // Update to your Azure SignalR endpoint
+        .WithAutomaticReconnect()
+        .Build();
+});
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
